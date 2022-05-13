62ee49e072487f1b5158681ccfee9b19c5d33c1b
Martin Yang <yjjfirst@hotmail.com>
2022-05-14 07:15:55 +0800

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#add domain and users
+./target/debug/fscmd domain add --name 45.76.77.24
+./target/debug/fscmd user add -d 1 -u 1000 -p gxGB418oc2TVU9rt
+./target/debug/fscmd user add -d 1 -u 1001 -p gxGB418oc2TVU9rt
+./target/debug/fscmd user add -d 2 -u 2000 -p gxGB418oc2TVU9rt
+./target/debug/fscmd user add -d 2 -u 2001 -p gxGB418oc2TVU9rt
+
+#add gateways
+./target/debug/fscmd gateway add --name kamailio_1001 --profile 2 --password 0z1*7CSUOj96nV5 --proxy 45.76.77.24 --register 45.76.77.24 --username 1001
+./target/debug/fscmd gateway add --name kamailio_1002 --profile 2 --password j1U4NyRN6LtTGzp --proxy 45.76.77.24 --register 45.76.77.24 --username 1002
