38a84cd215eff50150048fa7807463f7af492184
Martin Yang <yjjfirst@hotmail.com>
2022-10-08 14:58:11 +0800

    Extenion and domain unique in extension table

--- fslib/migrations/2021-05-26-070710_create_directory/up.sql
+++ fslib/migrations/2021-05-26-070710_create_directory/up.sql
@@ -1,9 +1,10 @@
 -- Your SQL goes here
 CREATE TABLE `extension` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `exten` VARCHAR(128) NOT NULL UNIQUE,
+  `exten` VARCHAR(128) NOT NULL,
   `exten_type` VARCHAR(64) NOT NULL,
   `domain_id` INT NOT NULL,
+  UNIQUE(exten, domain_id),
   PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
