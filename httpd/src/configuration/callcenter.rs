ff7925f3280a059043f7242c442770f619517eb4
Martin Yang <yjjfirst@hotmail.com>
2022-11-05 13:47:04 +0800

    Load queue correctly

--- httpd/src/configuration/callcenter.rs
+++ httpd/src/configuration/callcenter.rs
@@ -5,6 +5,9 @@ use crate::xml_utils::{start_element, end_element, attrs, param};
 use fslib::queue;
 
 pub fn serve<W: Write>(w: &mut EventWriter<W>) {
+    start_element(w, "document", attrs(vec![("type", "freeswitch/xml")]));
+    start_element(w, "section", attrs(vec![("name", "configuration")]));
+
     start_element(w,
                   "configuration",
                   attrs(vec![
@@ -13,12 +16,25 @@ pub fn serve<W: Write>(w: &mut EventWriter<W>) {
                   ]));
     settings(w);
     queues(w);
+    agents(w);
+    tiers(w);
 
+    end_element(w);
+    end_element(w);
     end_element(w);
 }
 
 pub fn settings <W: Write>(w: &mut EventWriter<W>) {
     start_element(w, "settings", None);
+    end_element(w);
+}
+
+pub fn agents <W: Write>(w: &mut EventWriter<W>) {
+    start_element(w, "agents", None);
+    end_element(w);
+}
+pub fn tiers <W: Write>(w: &mut EventWriter<W>) {
+    start_element(w, "tiers", None);
     end_element(w);
 }
 
