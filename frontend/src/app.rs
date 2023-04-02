9c0e3a7b957ab54746a5dca859f0d727542f5caf
Martin Yang <yjjfirst@hotmail.com>
2023-04-02 13:53:01 +0800

    Add tailwind css

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -3,20 +3,25 @@ use yew::prelude::*;
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <main>
-            <TopBanner></TopBanner>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello WASM" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-        </main>
+        <div class="flex">
+            <div>
+            <img/>
+            </div>
+            <div>
+                <h1>{"Welcome to NorthBy"}</h1>
+                <h2>{"A premium in sight and sound"}</h2>
+                <button>{"Learn More"}</button>
+            </div>
+        </div>
     }
 }
 
 #[function_component]
 pub fn  TopBanner() -> Html {
+    let banner = "This is top banner";
     html! {
         <div class="top_banner">
-        {"This is top banner"}
+        {banner}
         </div>
     }
 }
