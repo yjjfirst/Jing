ae75d9441aac96e9a91ce91cd7de163cadc37dd9
Martin Yang <yjjfirst@hotmail.com>
2023-10-08 11:31:55 +0800

    Used ID to identify ringing group

--- fs_httpd/src/api/ringing_group.rs
+++ fs_httpd/src/api/ringing_group.rs
@@ -14,31 +14,28 @@ pub fn ringing_group_config(cfg: &mut web::ServiceConfig) {
         .service(
             web::resource("/{id}/members")
                 .route(web::get().to(members)));
-
-
 }
 
-async fn index(path: web::Path<String>) -> impl Responder {
+async fn index(path: web::Path<i32>) -> impl Responder {
     let domain = path.into_inner();
-    let domain = domain::get_domain_by_name(domain).unwrap();
-
-    let groups = ringgroup::groups_in_domain(domain.id);
+    let groups = ringgroup::groups_in_domain(domain);
     web::Json(groups)
 }
 
-async fn get(info: web::Path<i32>) -> impl Responder {
-    let id = info.into_inner();
+async fn get(path: web::Path<(i32, i32)>) -> impl Responder {
+    let (_,id) = path.into_inner();
     let group = ringgroup::get_ringgroup(id).unwrap();
     web::Json(group)
 }
 
-async fn members(info: web::Path<i32>) -> impl Responder {
-    let id = info.into_inner();
+async fn members(path: web::Path<(i32, i32)>) -> impl Responder {
+    let (_, id) = path.into_inner();
     let members = ringgroup::members(id).unwrap();
     web::Json(members)
 }
 
-async fn update(info: web::Json<ringgroup::models::Ringgroup>) -> impl Responder {
+async fn update(group: web::Json<ringgroup::models::Ringgroup>) -> impl Responder {
 
+    println!("{:?}", group);
     web::Json("ok")
 }
