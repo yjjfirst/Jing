ac3b53d10ff07f75a86e4099d7cbb04af3d79dbf
Martin Yang <yjjfirst@hotmail.com>
2024-09-25 19:38:46 +0800

    Add charming crate for dashboard

--- frontend/src/pages/dashboard.rs
+++ frontend/src/pages/dashboard.rs
@@ -1,84 +1,170 @@
 
 use yew::prelude::*;
+use charming::{component::{Legend, Title}, df, element::{ItemStyle, Label},series::Pie, Chart, WasmRenderer};
+use charming::{component::Axis, element::AxisType, series::Bar};
+#[function_component]
+pub fn MemmoryCard() -> Html {
+    let f = yew_hooks::use_async::<_, _, ()>({
+        let chart = Chart::new()
+        .title(Title::new().text("Memory Usage"))
+        .legend(Legend::new().top("bottom"))
+        .series(
+            Pie::new()                
+                .name("Memory Chart")
+                .label(Label::new().show(false))
+                .radius(vec![30, 80])
+                .item_style(ItemStyle::new().border_radius(8))
+                .data(df![
+                    (80.0, "Used"),
+                    (20.0, "Free"),
+                ]),
+        );
+
+        let renderer = WasmRenderer::new(250, 250);
+        
+        async move {
+            renderer.render("memory_chart", &chart).unwrap();
+            Ok(())
+        }
+    });
+    
+    use_effect_with((), move |_| {
+        f.run();
+        || ()
+    });
+
+    html! {
+        <div id="memory_chart"></div>
+    }
+}
 
 #[function_component]
-pub fn DashboardCard() -> Html {
-    let r = 1..8;
-    html!{
-        <div class="overflow-x-auto">
-            <table class="table table-xs">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>{"Name"}</th>
-                        <th>{"Job"}</th>
-                        <th>{"location"}</th>
-                        <th>{"Favorite Color"}</th>
-                    </tr>
-                </thead>
-                <tbody>                      
-                {
-                r.into_iter().map(|r|{
-                    html!{
-                        <tr>
-                            <th>{r}</th>
-                            <td>{"Cy Ganderton"}</td>
-                            <td>{"Canada"}</td>
-                            <td>{"12/16/2020"}</td>
-                            <td>{"Blue"}</td>
-                        </tr>}
-                }).collect::<Html>()                       
-                }
-            
-                </tbody>
-            </table>
-        </div>
-    }
+pub fn CpuCard() -> Html {
+    let f = yew_hooks::use_async::<_, _, ()>({
+        let chart = Chart::new()
+        .title(Title::new().text("CPU Usage"))
+        .legend(Legend::new().top("bottom"))
+        .series(
+            Pie::new()                
+                .name("CPU Chart")
+                .radius(vec![30, 80])
+                .label(Label::new().show(false))
+                .item_style(ItemStyle::new().border_radius(8))
+                .data(df![
+                    (30.0, "Used"),
+                    (70.0, "Free"),
+                ]),
+        );
 
+        let renderer = WasmRenderer::new(250, 250);
+        
+        async move {
+            renderer.render("cpu_chart", &chart).unwrap();
+            Ok(())
+        }
+    });
+    
+    use_effect_with((), move |_| {
+        f.run();
+        || ()
+    });
+
+    html! {
+        <div id="cpu_chart"></div>
+    }
+}
+#[function_component]
+pub fn DiskCard() -> Html {
+    let f = yew_hooks::use_async::<_, _, ()>({
+        let chart = Chart::new()
+        .title(Title::new().text("Disk Usage"))
+        .legend(Legend::new().top("bottom"))
+        .series(
+            Pie::new()                
+                .name("Disk Chart")
+                .radius(vec![30, 80])
+                .label(Label::new().show(false))
+                .item_style(ItemStyle::new().border_radius(8))
+                .data(df![
+                    (40.0, "Used"),
+                    (60.0, "Free"),
+                ]),
+        );
+
+        let renderer = WasmRenderer::new(250, 250);
+        
+        async move {
+            renderer.render("disk_chart", &chart).unwrap();
+            Ok(())
+        }
+    });
+    
+    use_effect_with((), move |_| {
+        f.run();
+        || ()
+    });
+
+    html! {
+        <div id="disk_chart"></div>
+    }
+}
+
+#[function_component]
+pub fn CallStat() -> Html {
+        let f = yew_hooks::use_async::<_, _, ()>({
+            let chart = Chart::new()
+            .title(Title::new().text("Call Statistics"))
+            .x_axis(
+                Axis::new()
+                    .type_(AxisType::Category)
+                    .data(vec!["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]),
+            )
+            .y_axis(Axis::new().type_(AxisType::Value))
+            .series(Bar::new().data(vec![150, 230, 224, 218, 135, 147, 260]));
+    
+            let renderer = WasmRenderer::new(900, 400);
+            
+            async move {
+                renderer.render("call_chart", &chart).unwrap();
+                Ok(())
+            }
+        });
+        
+        use_effect_with((), move |_| {
+            f.run();
+            || ()
+        });
+    
+        html! {
+            <div id="call_chart"></div>
+        }
 }
 #[function_component]
 pub fn Dashboard() -> Html {
     html! {
-            <div class="flex flex-wrap">
-                <div class="w-1/2 bg-base-100 shadow-xl">
-                    <div class="card-body">
-                    <h2 class="card-title">{"User"}</h2>
-                        <DashboardCard />
-                        <div class="card-actions justify-end">
-                            <div class="badge badge-outline">{"User"}</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="w-1/2 bg-base-100 shadow-xl">
-                    <div class="card-body">
-                        <h2 class="card-title">
-                            {"Active Calls"}
-                        </h2>
-                        <DashboardCard />
-                    </div>
-                </div>
-
-                <div class="w-1/2 bg-base-100 shadow-xl">
-                    <div class="card-body">
-                        <h2 class="card-title">
-                            {"Recent calls"}
-                        </h2>
-                        <DashboardCard />
-                        <div class="card-actions justify-end">
-                            <div class="badge badge-outline">{"CDR"}</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="w-1/2 bg-base-100 shadow-xl">
-                    <div class="card-body">
-                        <h2 class="card-title">
-                            {"System"}
-                        </h2>
-                        <DashboardCard />
-                    </div>
+        <div>
+        <div class="flex flex-wrap">
+            <div class="w-1/3 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <MemmoryCard />
                 </div>
             </div>
-        }
+            <div class="w-1/3 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <CpuCard />
+                </div>
+            </div>
+            <div class="w-1/3 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <DiskCard />
+                </div>
+            </div>
+        </div>
+        <div class="w-3/3 bg-base-100 shadow-xl">
+            <div class="card-body">
+                <CallStat />
+            </div>
+        </div>
+        </div>
+    }
 }
