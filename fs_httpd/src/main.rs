c8016a95dd09c78c33ffc8da5e2915bbb8f76283
Martin Yang <yjjfirst@hotmail.com>
2026-04-27 03:34:46 +0000

    Add TLS support

--- fs_httpd/src/main.rs
+++ fs_httpd/src/main.rs
@@ -1,7 +1,9 @@
 mod fs;
 mod api;
 mod cdr;
+
 use actix_web::{web, App, HttpServer};
+use rustls::{ServerConfig};
 use actix_cors::Cors;
 use actix_files::{Files, NamedFile};
 use actix_web::{
@@ -10,6 +12,9 @@ use actix_web::{
     middleware::{from_fn, Next},
     Error, Result
 };
+use std::fs::File;
+use std::io::BufReader;
+
 use api::{api_config};
 use fs_lib::portal_token::is_expired;
 
@@ -52,6 +57,8 @@ async fn index() -> Result<NamedFile, Error> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let config = load_rustls_config();
+
     HttpServer::new(||{
         let cors = Cors::permissive();
         App::new()
@@ -64,7 +71,27 @@ async fn main() -> std::io::Result<()> {
             .service(Files::new("/", "/var/www/pbx").index_file("index.html"))
             .default_service(web::route().to(index))
     })
-        .bind("0.0.0.0:9090")?
+        .bind_rustls_0_23("137.220.37.143:9090", config)?
+        .bind("127.0.0.1:9090")?
         .run()
         .await
+}
+
+
+fn load_rustls_config() -> ServerConfig {
+    // Example helper to load cert.pem and key.pem
+    let cert_file = &mut BufReader::new(File::open("/etc/letsencrypt/live/pbx.telman.me/cert.pem").unwrap());
+    let key_file = &mut BufReader::new(File::open("/etc/letsencrypt/live/pbx.telman.me/privkey.pem").unwrap());
+
+    let cert_chain = rustls_pemfile::certs(cert_file)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    let mut keys = rustls_pemfile::pkcs8_private_keys(key_file)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+
+    ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(cert_chain, keys.remove(0).into())
+        .expect("bad certificate/key")
 }
