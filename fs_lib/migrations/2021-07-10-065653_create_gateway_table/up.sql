52d713b558568a4a23683a4b6f9e44d5043d0fce
Martin Yang <yjjfirst@hotmail.com>
2026-08-06 02:36:37 +0000

    Clean up migration

--- fs_lib/migrations/2021-07-10-065653_create_gateway_table/up.sql
+++ fs_lib/migrations/2021-07-10-065653_create_gateway_table/up.sql
@@ -3,10 +3,6 @@ CREATE TABLE gateways (
 id SERIAL,
 profile_id INT NOT NULL,
 gateway_name VARCHAR(256) NOT NULL,
-proxy VARCHAR(256) NOT NULL,
-register VARCHAR(256) NOT NULL,
-username VARCHAR(256),
-password VARCHAR(256),
 PRIMARY KEY(id),
 FOREIGN KEY(profile_id) REFERENCES profiles(id)
 );
--- fs_lib/migrations/2026-05-18-030010-0000_remove_params_column_from_gateway_table/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-ALTER TABLE gateways
-DROP COLUMN proxy,
-DROP COLUMN register,
-DROP COLUMN username,
-DROP COLUMN password;
