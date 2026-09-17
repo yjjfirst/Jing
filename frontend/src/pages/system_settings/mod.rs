f8fdc7f2a80c3cb3d355e9df5cec61114fd3ccd9
Martin Yang <yjjfirst@hotmail.com>
2026-09-17 03:13:56 +0000

    Set system settings from frontend

--- frontend/src/pages/system_settings/mod.rs
+++ frontend/src/pages/system_settings/mod.rs
@@ -1,11 +1,13 @@
 pub mod model;
 
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
 use yew::prelude::*;
 use yew_router::prelude::*;
 use yewdux::prelude::*;
+use serde::{Serialize, Deserialize};
 
 use crate::components::header::Header;
+use crate::components::action_buttons::ActionButtons;
 use crate::models::Service;
 use crate::store::Store;
 
@@ -18,7 +20,7 @@ pub enum SystemSettingsRoute {
 }
 
 #[component]
-pub fn SystemSettingsPage() -> Html {
+pub fn SystemSettings() -> Html {
     let system_settings: UseStateHandle<BTreeMap<String, Vec<SystemSetting>>> = 
         use_state(|| BTreeMap::new());
     let (store, _) = use_store::<Store>();
@@ -36,21 +38,65 @@ pub fn SystemSettingsPage() -> Html {
         });
     }
 
+    let handle_submit = {
+        #[derive(Serialize, Deserialize, Debug, Clone)]
+        pub struct Setting {
+            pub setting_key: String,
+            pub setting_value: String,
+        };
+
+        let store = store.clone();
+        Callback::from(move |e: SubmitEvent| {
+            e.prevent_default();
+            let store = store.clone();
+            let form = e.target_unchecked_into::<web_sys::HtmlFormElement>();
+            let form_data = web_sys::FormData::new_with_form(&form).unwrap();
+            let mut settings_map: HashMap<String, Vec<Setting>> = HashMap::new();
+            let entries = js_sys::try_iter(&form_data).unwrap().unwrap();
+            for entry in entries {
+                let entry = entry.unwrap();
+                let array = js_sys::Array::from(&entry);
+                let key = array.get(0).as_string().unwrap();
+                let (section, setting_key) = key.split_once("-").unwrap();
+                let value = array.get(1).as_string().unwrap();
+                settings_map.entry(section.to_string())
+                        .or_insert_with(Vec::new)
+                        .push(Setting {
+                            setting_key: setting_key.to_string(),
+                            setting_value: value,
+                });
+            }
+            wasm_bindgen_futures::spawn_local(async move {
+                Service::post("/system-settings", store.selected_domain_id, settings_map)
+                    .await
+                    .unwrap();
+            });
+        })
+    };
+
     html! {
         <div class="grow mr-2">
             <Header title="System Settings"></Header>
             <div class="divider my-1"></div>
-            for (section, settings) in system_settings.iter() {
-                <fieldset class="fieldset bg-base-200 border-base-300 rounded-box border p-4 text-base">
-                    <legend class="fieldset-legend">{section.clone()}</legend>
-                    for setting in settings {
+            <form onsubmit={handle_submit.clone()}>
+                for (section, settings) in system_settings.iter() {
+                    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box border p-4 text-base">
+                        <legend class="fieldset-legend">{section.clone()}</legend>
                         <div class="grid grid-cols-3 gap-1">
-                            <label class="pbx-label">{setting.setting_key.clone()}</label>
-                            <input type="text" class="pbx-input" placeholder={setting.setting_value.clone()} />
+                            for setting in settings {
+                                <label class="pbx-label">{setting.setting_key.clone()}</label>
+                                <input 
+                                    type="text" 
+                                    class="pbx-input" 
+                                    name={format!("{}-{}", section, setting.setting_key.clone())}
+                                    value={setting.setting_value.clone()} 
+                                />
+                            }
                         </div>
-                    }
-                </fieldset>                 
-            }
+                    </fieldset>                 
+                }
+                <ActionButtons  has_cancel={false}/>
+            </form>
         </div>
     }
 }
@@ -58,7 +104,7 @@ pub fn SystemSettingsPage() -> Html {
 pub fn system_settings_switch(route: SystemSettingsRoute) -> Html {
     match route {
         SystemSettingsRoute::Index => html!{
-            <SystemSettingsPage />
+            <SystemSettings />
         }
     }
 }
--- jhttpd/src/api/system_setting.rs
+++ jhttpd/src/api/system_setting.rs
@@ -5,9 +5,7 @@ use serde::{Serialize, Deserialize};
 use super::Status;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct SystemSetting {
-    pub setting_id: i32,
-    pub setting_section: String,
+pub struct PostSetting {
     pub setting_key: String,
     pub setting_value: String,
 }
@@ -31,13 +29,12 @@ async fn index() -> impl Responder {
      web::Json(settings_map)
 }
 
-async fn post(body: web::Json<SystemSetting>) -> impl Responder {
-    let setting = body.into_inner();
-    match system_setting::update(&setting.setting_section, 
-        &setting.setting_key, 
-        &setting.setting_value) 
-    {
-        Ok(_) => web::Json(Status { status: "Ok".to_string() }),
-        Err(_) => web::Json(Status { status: "Error".to_string() }),
+async fn post(body: web::Json<HashMap<String, Vec<PostSetting>>>) -> impl Responder {
+    let settings = body.into_inner();
+    for (section, settings) in settings {
+        for s in settings {
+            system_setting::update(&section, &s.setting_key, &s.setting_value).unwrap();
+        }
     }
+    web::Json(Status {status: "Ok".to_string()})
 }
