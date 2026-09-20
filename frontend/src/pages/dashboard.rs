46fe81de6845170711f06dfd7397c995738a6fc6
Martin Yang <yjjfirst@hotmail.com>
2026-09-20 12:38:25 +0000

    Show memory chart correctly

--- frontend/src/pages/dashboard.rs
+++ frontend/src/pages/dashboard.rs
@@ -10,18 +10,33 @@ use charming::{component::{Title}, df, element::{ItemStyle, Label, Tooltip},seri
 use charming::{component::Axis, element::AxisType, series::Bar};
 #[function_component]
 pub fn MemmoryCard() -> Html {
+    #[derive(Serialize, Deserialize, Clone)]
+    pub struct MemInfo {
+        pub free: f64,
+        pub buffers: f64,
+        pub cached: f64
+    }
+    let mem_info = use_state(||MemInfo{
+        free: 0.0, buffers: 0.0, cached: 0.0
+    });
+    let (store,_) = use_store::<Store>();
     let f = yew_hooks::use_async::<_, _, ()>({
+        let mem_info = mem_info.clone();
         let chart = Chart::new()
         .title(Title::new().text("Memory"))
+        .tooltip(Tooltip::new()
+            .formatter("{a}<br/> {b}: {c}M")
+        )
         .series(
             Pie::new()
-                .name("Memory Chart")
+                .name("Memory")
                 .label(Label::new().show(false))
                 .radius(vec![30, 80])
                 .item_style(ItemStyle::new().border_radius(8))
                 .data(df![
-                    (80.0, "Used"),
-                    (20.0, "Free"),
+                    (((*mem_info).buffers/1000.0).trunc(), "Buffers"),
+                    (((*mem_info).free/1000.0).trunc(), "Free"),
+                    (((*mem_info).cached/1000.0).trunc(), "Cached"),
                 ]),
         );
 
@@ -32,12 +47,40 @@ pub fn MemmoryCard() -> Html {
             Ok(())
         }
     });
-
-    use_effect_with((), move |_| {
-        f.run();
-        || ()
-    });
-
+    {
+        let mem_info = mem_info.clone();
+        let store = store.clone();
+        let f = f.clone();
+        use_effect_with((), move |_| {
+            let mem_info = mem_info.clone();
+            let store = store.clone();
+            let f = f.clone();
+            spawn_local(async move{
+                let mem_fetched: MemInfo = Service::get("/system-info/memory", store.selected_domain_id)
+                    .await
+                    .unwrap();
+                mem_info.set(mem_fetched);
+                f.run()
+            });
+        });
+    }
+    {
+        let mem_info = mem_info.clone();
+        let store = store.clone();
+        let f = f.clone();
+        use_interval(move || {
+            let mem_info = mem_info.clone();
+            let store = store.clone();
+            let f = f.clone();
+            spawn_local(async move{
+                let mem_fetched: MemInfo = Service::get("/system-info/memory", store.selected_domain_id)
+                    .await
+                    .unwrap();
+                mem_info.set(mem_fetched);
+                f.run()
+            });
+        }, 10 * 1000)
+    }
     html! {
         <div id="memory_chart"  class="m-1"></div>
     }
@@ -132,7 +175,9 @@ pub fn DiskCard() -> Html {
         let free = free.clone();
         let chart = Chart::new()
         .title(Title::new().text("Disk"))
-        .tooltip(Tooltip::new())
+        .tooltip(Tooltip::new()
+            .formatter("{a}<br/> {b}: {c}G")
+        )
         .series(
             Pie::new()
                 .name("Disk")
